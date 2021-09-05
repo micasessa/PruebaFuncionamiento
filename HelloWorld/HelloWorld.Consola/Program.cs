371e4ea4ca59ebaf8f69081d1f6bf09b4868288b
micasessa <micasessa@yahoo.com>
2021-09-05 12:29:44 -0300

    prueba 1

---
+++ HelloWorld/HelloWorld.Consola/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HelloWorld.Libreria;
+
+namespace HelloWorld.Consola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string frase = "Hola Mundo";
+            Console.WriteLine("Imprimir lo siguiente:");
+            Console.WriteLine(frase);
+
+        }
+    }
+}
